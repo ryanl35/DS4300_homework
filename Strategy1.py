e9e119ca5fd57301f9be711189467203c8d2e077
chen-v <chen.vi@husky.neu.edu>
2020-01-26 10:32:48 -0500

    add tweets to redis

--- Strategy1.py
+++ Strategy1.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 
-import pandas as pd
 import time
+import redis
+import csv
+
+con = redis.Redis(host='localhost', port=6379, db = 0)
 
 def postTweet():
-      tweets = pd.read_csv("generatedTweets.csv")
-      # tweets = tweets.drop ??
-      print("HERE")
-
-      iteration = 0
-      for i, row in tweets.iterrows():
-        print(row['tweet_text'])
-        if iteration == 100:
-            break
-        iteration += 1
+    with open("generatedTweets.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # the below statement will skip the first row
+        next(csv_reader)
+        i=0
+        dict = {}
+        for lines in csv_reader:
+            time = i
+            text = lines[3]
+            dict = {text:time}
+            con.zadd('tweets',dict)
+            i += 1
 
 postTweet()
