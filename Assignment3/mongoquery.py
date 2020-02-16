0190b29cd911db9d7e181446bb5dcd7466b7af82
Ryan Liang <ryanliang@Ryans-MacBook-Air.local>
2020-02-16 16:56:36 -0500

    made API

--- Assignment3/mongoquery.py
+++ Assignment3/mongoquery.py
@@ -8,30 +8,33 @@ client = MongoClient('mongodb://localhost:27017/')
 
 
 # Part 1
-products.productCatalog.find({diameter: "44mm", brand: "Tommy Hilfiger", color: "beige"})
+#products.productCatalog.find({diameter: "44mm", brand: "Tommy Hilfiger", color: "beige"})
 
 
 
 
 # Part 2 - Search API
 
-# create the two lists
-names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
-ages = ["13", "13", "18", "22", "100"]
+class MongoDBClass ():
 
-# create a Data Frame from a dictionary that has the above information in it
-names_ages = {"Name": names, "Age": ages}
-ds4300 = pd.DataFrame(names_ages)
+    def addPeopleToDatabase():
+        # create the two lists
+        names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
+        ages = ["13", "13", "18", "22", "100"]
 
-# assign db to the database "assignment3"
-db = client["assignment3"]
-# assign assignment3 to the collection inside of db "assignment3" titled "names"
-assignment3 = db["names"]
+        # create a Data Frame from a dictionary that has the above information in it
+        names_ages = {"Name": names, "Age": ages}
+        ds4300 = pd.DataFrame(names_ages)
+
+        # assign db to the database "assignment3"
+        db = client["assignment3"]
+        # assign assignment3 to the collection inside of db "assignment3" titled "names"
+        assignment3 = db["names"]
 
 
-# add the name and age into the MongoDB database
-for index, row in ds4300.iterrows():
-    mydict = { "Name": row["Name"], "Age" : row["Age"] }
-    x = assignment3.insert_one(mydict)
+        # add the name and age into the MongoDB database
+        for index, row in ds4300.iterrows():
+            mydict = { "Name": row["Name"], "Age" : row["Age"] }
+            x = assignment3.insert_one(mydict)
 
-#db.myCollection.find( {   name: "john"   } )
+    #db.myCollection.find( {   name: "john"   } )
