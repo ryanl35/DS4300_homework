65440c6283830fac3ca4791273c7e33f2bbe5991
Ryan Liang <ryanliang@Ryans-MacBook-Air.local>
2020-02-20 20:51:53 -0500

    finished API

--- Assignment3/mongoquery.py
+++ Assignment3/mongoquery.py
@@ -11,20 +11,18 @@ client = MongoClient('mongodb://localhost:27017/')
 #products.productCatalog.find({diameter: "44mm", brand: "Tommy Hilfiger", color: "beige"})
 
 
-
-
 # Part 2 - Search API
 
 class MongoDBClass:
 
-    def addPeopleToDatabase():
-        # create the two lists
-        names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
-        ages = ["13", "13", "18", "22", "100"]
+    def addPeopleToDatabase(self, name, age):
+        # # create the two lists
+        # names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
+        # ages = ["13", "13", "18", "22", "100"]
 
-        # create a Data Frame from a dictionary that has the above information in it
-        names_ages = {"Name": names, "Age": ages}
-        ds4300 = pd.DataFrame(names_ages)
+        # # create a Data Frame from a dictionary that has the above information in it
+        # names_ages = {"Name": name, "Age": age}
+        # ds4300 = pd.DataFrame(names_ages)
 
         # assign db to the database "assignment3"
         db = client["assignment3"]
@@ -33,8 +31,27 @@ class MongoDBClass:
 
 
         # add the name and age into the MongoDB database
-        for index, row in ds4300.iterrows():
-            mydict = { "Name": row["Name"], "Age" : row["Age"] }
-            x = assignment3.insert_one(mydict)
+        # for index, row in ds4300.iterrows():
+        #     mydict = { "Name": row["Name"], "Age" : row["Age"] }
+        #     x = assignment3.insert_one(mydict)
 
-    #db.myCollection.find( {   name: "john"   } )
+        # inserts the parameters into a dictionary
+        mydict = { "Name": name, "Age" : age }
+        # inserts the dictionary into the database
+        x = assignment3.insert_one(mydict)
+
+    def queryDatabase(self, field, query):
+        
+        db = client["assignment3"]
+        assignment3 = db["names"]
+
+        myfind = assignment3.find({field: query})
+
+        for x in myfind:
+            print(x)
+
+
+
+# Testing to make sure it works
+# MongoDBClass.addPeopleToDatabase(MongoDBClass, "Vivian", 13)
+# MongoDBClass.queryDatabase(MongoDBClass, "Name", "Vivian")
