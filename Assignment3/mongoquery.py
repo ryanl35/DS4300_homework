d2ee98dd79b6878de5d7929d300c398c51ed845f
Ryan Liang <ryanliang@Ryans-MacBook-Air.local>
2020-02-21 09:35:10 -0500

    finished commit

--- Assignment3/mongoquery.py
+++ Assignment3/mongoquery.py
@@ -15,35 +15,51 @@ client = MongoClient('mongodb://localhost:27017/')
 
 class MongoDBClass:
 
-    def addPeopleToDatabase(self, name, age):
-        # # create the two lists
-        # names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
-        # ages = ["13", "13", "18", "22", "100"]
+    # def addPeopleToDatabase(self, name, age):
+    #     # # create the two lists
+    #     # names = ["Vivian", "Hiren", "Ryan", "John", "ChanMin"]
+    #     # ages = ["13", "13", "18", "22", "100"]
 
-        # # create a Data Frame from a dictionary that has the above information in it
-        # names_ages = {"Name": name, "Age": age}
-        # ds4300 = pd.DataFrame(names_ages)
+    #     # # create a Data Frame from a dictionary that has the above information in it
+    #     # names_ages = {"Name": name, "Age": age}
+    #     # ds4300 = pd.DataFrame(names_ages)
+
+    #     # assign db to the database "assignment3"
+    #     db = client["assignment3"]
+    #     # assign assignment3 to the collection inside of db "assignment3" titled "names"
+    #     assignment3 = db["names"]
+
+
+    #     # add the name and age into the MongoDB database
+    #     # for index, row in ds4300.iterrows():
+    #     #     mydict = { "Name": row["Name"], "Age" : row["Age"] }
+    #     #     x = assignment3.insert_one(mydict)
+
+    #     # inserts the parameters into a dictionary
+    #     mydict = { "Name": name, "Age" : age }
+    #     # inserts the dictionary into the database
+    #     x = assignment3.insert_one(mydict)
+
+    def addProductToDatabase(self, product, attribute, attibuteValue):
 
         # assign db to the database "assignment3"
         db = client["assignment3"]
         # assign assignment3 to the collection inside of db "assignment3" titled "names"
-        assignment3 = db["names"]
-
-
-        # add the name and age into the MongoDB database
-        # for index, row in ds4300.iterrows():
-        #     mydict = { "Name": row["Name"], "Age" : row["Age"] }
-        #     x = assignment3.insert_one(mydict)
+        assignment3 = db["products"]
 
         # inserts the parameters into a dictionary
-        mydict = { "Name": name, "Age" : age }
+        # the dictionary contains the input parameter "attribute" as what the value is describing
+        mydict = { "Product": product, attribute : attibuteValue }
         # inserts the dictionary into the database
         x = assignment3.insert_one(mydict)
 
-    def queryDatabase(self, field, query):
+    def queryDatabase(self, dbname, colname, field, query):
         
-        db = client["assignment3"]
-        assignment3 = db["names"]
+        # dbname is the name of the database the user wishes to query from
+        db = client[dbname]
+
+        # colname is the name of the collection within that database that the user wishes to query from
+        assignment3 = db[colname]
 
         myfind = assignment3.find({field: query})
 
@@ -55,3 +71,8 @@ class MongoDBClass:
 # Testing to make sure it works
 # MongoDBClass.addPeopleToDatabase(MongoDBClass, "Vivian", 13)
 # MongoDBClass.queryDatabase(MongoDBClass, "Name", "Vivian")
+
+# MongoDBClass.addProductToDatabase(MongoDBClass, "Table", "Weight", "44lbs")
+MongoDBClass.queryDatabase(MongoDBClass, "assignment3", "products", "Weight", "44lbs")
+
+
